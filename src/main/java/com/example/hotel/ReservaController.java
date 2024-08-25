010f61ba6a18eb69018d8e15a67cc6acc9492f66
Bernardo <bcbs1986@gmail.com>
2024-08-24 23:42:37 -0400

    2.0

--- src/main/java/com/example/hotel/ReservaController.java
+++ src/main/java/com/example/hotel/ReservaController.java
@@ -16,11 +16,12 @@ public class ReservaController {
     public ReservaController(){
         reserva.add(new Reserva(1, "Bernardo Bravo", "25/03/2024", "12:30", "", "","Doble", "No", "Efectivo", "987654321", "mail@mail.com", Arrays.asList(new Habitacion("100A", "1", "limpia"), new Habitacion("101A", "2", "sucia")), llenarListaDisp("100A", "Disponible") ));
         reserva.add(new Reserva(2, "Francisca Urbina", "26/01/2024", "13:30", "", "","Simple", "No", "Tarjeta de Débito", "987654321", "mail@mail.com",Arrays.asList(new Habitacion("100A", "1", "limpia")), llenarListaDisp("100A", "Disponible")));
-        // reserva.add(new Reserva(3, "Laura Alegria", "28/02/2024", "14:30", "", "","Matrimonial", "No", "Efectivo", "987654321", "mail@mail.com", llenarListaDisp("100A", "Disponible")));
-        // reserva.add(new Reserva(4, "Paz Duran", "12/11/2022", "15:30", "", "","Doble", "No", "Tarjeta de Crédito", "987654321", "mail@mail.com", llenarListaDisp("100A", "Disponible")));
-        // reserva.add(new Reserva(5, "Oriana Yañez", "15/03/2024", "16:30", "", "","Simple", "No", "Efectivo", "987654321", "mail@mail.com", llenarListaDisp("100A", "Disponible")));
-        // reserva.add(new Reserva(6, "Patricio Urzua", "01/08/2023", "17:30", "", "","Matrimonial", "No", "Efectivo", "987654321", "mail@mail.com", llenarListaDisp("100A", "Disponible")));
-        // reserva.add(new Reserva(7, "Fatima Saez", "20/11/2023", "18:30", "", "","Doble", "No", "Tarjeta de Crédito", "987654321", "mail@mail.com", llenarListaDisp("100A", "Disponible")));
+        reserva.add(new Reserva(3, "Laura Alegria", "28/02/2024", "14:30", "", "","Matrimonial", "No", "Efectivo", "987654321", "mail@mail.com", Arrays.asList(new Habitacion("100A", "1", "limpia")), llenarListaDisp("100A", "Disponible")));
+        reserva.add(new Reserva(4, "Paz Duran", "12/11/2022", "15:30", "", "","Doble", "No", "Tarjeta de Crédito", "987654321", "mail@mail.com", Arrays.asList(new Habitacion("100A", "1", "limpia")),  llenarListaDisp("100A", "Disponible")));
+        reserva.add(new Reserva(5, "Oriana Yañez", "15/03/2024", "16:30", "", "","Simple", "No", "Efectivo", "987654321", "mail@mail.com", Arrays.asList(new Habitacion("100A", "1", "limpia")),  llenarListaDisp("100A", "Disponible")));
+        reserva.add(new Reserva(6, "Patricio Urzua", "01/08/2023", "17:30", "", "","Matrimonial", "No", "Efectivo", "987654321", "mail@mail.com", Arrays.asList(new Habitacion("100A", "1", "limpia")),  llenarListaDisp("100A", "Disponible")));
+        reserva.add(new Reserva(7, "Fatima Saez", "20/11/2023", "18:30", "", "","Doble", "No", "Tarjeta de Crédito", "987654321", "mail@mail.com", Arrays.asList(new Habitacion("100A", "1", "limpia")),  llenarListaDisp("100A", "Disponible")));
+        reserva.add(new Reserva(8, "Juan Perez", "20/12/2023", "18:30", "", "","Doble", "No", "Tarjeta de Crédito", "987654321", "mail@mail.com", Arrays.asList(new Habitacion("100A", "1", "limpia")),  llenarListaDisp("100A", "Disponible")));
     }
 
     private Map<String, List<String>> llenarListaDisp(String habitacion, String disponibilidad){
